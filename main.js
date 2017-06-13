6b4bf5943fdac2e61f661a2eba4106b52d6ff365
punktuur <Sjoerd.paardekooper@gmail.com>
2017-06-13 17:20:36 +0200

    hello

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 var canvas = d3.select('#map').append('svg')
-	.attr('width', 960)
-	.attr('height', 960)
+	.attr('width', 1170)
+	.attr('id', 'kaart')
 
-d3.json('../data/buurten_amsterdam_wsg84_stadsdelen.geojson', function (data) {
-	// maak groep ('g') en bind data 
-	var group = canvas.selectAll('g')
-		.data(data.features)
-		.enter()
-		.append('g');
+var projection = d3.geo.mercator().scale(150000).translate([-12350, 162000]);
+var path = d3.geo.path().projection(projection);
 
-	var projection = d3.geo.mercator().scale(150000).translate([-12350, 162000]);
-	var path = d3.geo.path().projection(projection);
+// **********
+// FUNCTIONS
+// **********
 
-	// color scale
-	var color_scale = d3.scale.linear().domain([0, 0.5]).range(['#e5f5f9','#99d8c9', '#2ca25f'])
-
+function updateData(data, group, key) {
+	array_key_data = Object.values(data).map(function(i){return i[key]})
+	var max = Math.max.apply(Math, array_key_data)
+	var min = Math.min.apply(Math, array_key_data)
+	var color_scale = d3.scale.linear().domain([min, max]).range(['#e5f5f9','#99d8c9', '#2ca25f'])
 	var areas = group.append("path")
 		.attr('d', path)
 		.attr('class', 'area')
 		.attr('fill', function(d) {
-			var percentage = (d.properties.nietwesters/d.properties.totaal)
-			var count = 0
-			if (isNaN(percentage)) {
-				return 'white'
-			} else
-			count = count + 1
-			console.log(count)
-			return(color_scale(percentage))
+			postcode_geo_data = d.properties.postcode
+			data_per_buurt = data[postcode_geo_data]
+			console.log(data_per_buurt)
+			console.log(key)
+			// als er een plek is zonder postcode
+			if (data_per_buurt === undefined) {
+				var red = '#FF0000'
+				return red
+			} else {
+				// als er een plek is met postcode
+				value = data_per_buurt[key]
+				return color_scale(value)
+
+			}
 		})
 		.attr('id', function(d){ return d.properties.buurtcom00})
-		//.on('click', function(d) { drawPiechart(d.properties)})
-})
+}
+
+
+d3.json('../data/buurten_amsterdam_wsg84_stadsdelen_zip.geojson', function (data_geo) {
+	// maak groep ('g') en bind data 
+	var group = canvas.selectAll('g')
+		.data(data_geo.features)
+		.enter()
+		.append('g');
+
+	d3.json('../data/price_reviews.json', function(data_buurten) {
+		d3.select('#dropdown')
+			.on('change', function() {
+				var key = d3.select(this)[0][0].value
+				console.log(key)
+				updateData(data_buurten, group, key);
+
+		});
+
+	});
+
+});
+
+
 
--- process-report.md
+++ process-report.md
@@ -1,13 +1,18 @@
-# Process report 
+#Process report 
 
-12 juni 2017
+### 12 juni 2017
 Data was tot nu toe enkel beschikbaar per buurt maar kon de koppeling niet leggen tussen de buurt in de data en de buurt op de kaart. Dat is nu wel gedaan door middel van de eerste 4 cijfers van een postcode. Op internet stond een tabel waar de postcodes per buurt worden gegeven. Vervolgens via python de Geojson file die verantwoordelijk is voor het tekenen van de kaart aangepast dat per buurt er nu ook een postcode bij zit waardoor ik dan alle data via de postcode kan linken aan de buurt. Veel tijd besteed om een manier te vinden hoe ik dit het beste kon doen. 
 
 Voor morgen wil ik graag de data in de map laten zien, een layout maken, verschillende soorten data met een dropdown laten zien. 
 
-13 juni 2017
+### 13 juni 2017
+Dropdown en het veranderen van de data op de kaart van Amsterdam werkt. Eventuele probleemgebieden waren de callbacks. Verder framework gemaakt met 3 lagen van 960 width. 
 
+Morgen: 
+ - histogram maken
+ - interactiviteit tussen histogram & kaart?? 
 
+ 
 
 
 
