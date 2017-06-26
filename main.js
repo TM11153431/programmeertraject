0884fda3bd53318bb63d48e6159e21dfefbfbdbc
punktuur <Sjoerd.paardekooper@gmail.com>
2017-06-26 12:38:47 +0200

    hello

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-// global variabels
-var key = 'number_of_reviews'
+// Naam: Sjoerd Paardekooper
+// Studend-id: 10278397
+// Vak: Programmeerproject aan de Universiteit van Amsterdam
+// Docent: drs. M.S. Stegeman (coördinator)
+
+// Naam file: main.js
+// Beschrijving file: hierin staat alle javascript vernatwoordelijk voor de d3 visualisatie. Wordt aangeroepen vanuit de index file. 
+
+// ---------------------------------------------------------
+
+
+// ********************
+// Global variables
+// ********************
+data_type = 'number_of_reviews'
+selected = 0
 
 // SVG variabelen
 var svg_kaart = d3.select('#map').append('svg')
@@ -7,17 +21,26 @@ var svg_kaart = d3.select('#map').append('svg')
 	.attr('height', 390);
 
 var svg_histogram = d3.select('#histogram').append('svg')
-	.attr('width', 400)
-	.attr('height', 150);
+	.attr('width', 500)
+	.attr('height', 150)
+	.style('padding-left', '50px')
+	.style('padding-right', '50px')
+	.style('padding-top', '20px');
 
-var show_data = svg_kaart.append('text')
+var show_data = d3.select('.container').append('text')
 	.attr('x', 500)
-	.attr('y', 20)
+	.attr('y', 50)
 	.attr('id', 'show_data')
-	.text(0)
+	.text(' ')
 
-var svg_piechart1 = d3.select('#bottom').append('svg').attr('id', 'piechart1')
-var svg_piechart2 = d3.select('#bottom').append('svg').attr('id', 'piechart2')
+var show_buurt = d3.select('.container').append('text')
+	.attr('x', 500)
+	.attr('y', 400)
+	.attr('id', 'show_buurt')
+	.text(' ')
+
+var svg_piechart1 = d3.select('#piechart').append('svg').attr('id', 'piechart1')
+var svg_piechart2 = d3.select('#piechart').append('svg').attr('id', 'piechart2')
 
 var scale = 0.70
 var projection = d3.geo.mercator().scale(125800*scale).translate([-10350*scale, 135670*scale]);
@@ -27,38 +50,41 @@ var path = d3.geo.path().projection(projection);
 // DRAW FUNCTIONs
 // **********
 
-function kaart(data, buurten, key) {
+// Tekent een kaart van Amsterdam Mercator style aan de hand van een geojson file. 
+function kaart(data, buurten, data_type) {
 
 	// verwijder alle buurten
-	d3.selectAll('.buurten').remove();
+	d3.selectAll('.buurt').selectAll('path').remove();
 
-	var color_scale = bereken_kaart_scale(data, key)
+	var color_scale = bereken_scale(data, data_type)
 
+	// teken alle buurten
 	var areas = buurten.append("path")
 		.attr('d', path)
 		.attr('class', 'area')
+		.attr('id', function(d){return(d.properties.postcode)})
 		.attr('fill', function(d) {
 			postcode_geo_data = d.properties.postcode
 			data_per_buurt = data[postcode_geo_data]
 			// als er een plek is zonder postcode
 			if (data_per_buurt === undefined) {
-				var red = '#FF0000'
+				var red = '#FFFFFF'
 				return red
 			} else {
-				// als er een plek is met postcode
-				value = data_per_buurt[key]
+			// als er een plek is met postcode
+				value = data_per_buurt[data_type]
 				return color_scale(value)
 			}
-		})
-		//.attr('id', function(d){ return d.properties.postcode })
-}
+		});
+};
 
-function histogram(data,svg_histogram, key) {
+// Histogram functie gemaakt aan de hand van buurten data
+function histogram(data,svg_histogram, data_type) {
 
 	svg_histogram.selectAll('rect').remove()
 	svg_histogram.selectAll('g').remove()
 
-	var data_sorted = Object.keys(data).map(function(d) {return [d, data[d][key]]}).sort(function(a,b){return(b[1]-a[1])})
+	var data_sorted = Object.keys(data).map(function(d) {return [d, data[d][data_type]]}).sort(function(a,b){return(b[1]-a[1])})
 	values = data_sorted.map(function(d){return d[1]})
 
 	// input
@@ -75,37 +101,55 @@ function histogram(data,svg_histogram, key) {
 	//define scales 
 	var y_scale = d3.scale.linear().domain([d3.min(values), d3.max(values)]).range([0, height]);
 	var x_scale = d3.scale.ordinal().domain(keys).rangeRoundPoints([0,width])
-	var color_scale = d3.scale.linear().domain([min, max]).range(['#e5f5f9', '#99d8c9', '#2ca25f'])
+	var color_scale = bereken_scale(data, data_type)
 
-	var bars = svg_histogram.selectAll('g')
+	var y_scale_axis = d3.scale.linear().domain([d3.min(values), d3.max(values)]).range([height, 0]);
+
+	//define y-axis
+	var yAxis = d3.svg.axis()
+		.scale(y_scale_axis)
+		.orient('left')
+		.tickValues([d3.max(values)])
+
+	// append y axis
+	svg_histogram.append('g')
+		.attr('class', 'y axis')
+	 	.call(yAxis)
+
+	var bars = svg_histogram.selectAll('bar')
 		.data(Object.keys(data))
 		.enter()
 		.append('rect')
 		.classed('bin', true)
 		.attr('position', 'relative')
 		.attr('x', function(d) { return x_scale(d)})
-		.attr('y', function(d) { return (height - y_scale(data[d][key])) })
-		.attr('height', function(d){ return y_scale((data[d][key]))})
+		.attr('y', function(d) { return (height - y_scale(data[d][data_type])) })
+		.attr('height', function(d){ return y_scale((data[d][data_type]))})
 		.attr('width', bar_width)
-		.attr('fill', function(d) { return( color_scale(data[d][key])) })
+		.attr('fill', function(d) { return( color_scale(data[d][data_type])) })
 		.attr('id', function(d){return('pc'+d)})
 	
 }
 
-function piechart(data, svg_piechart, key ) {
+function piechart(data, svg_piechart, data_type ) {
+
+	svg_piechart.select('g').remove()
 
 	// svg_piechart.selectAll().remove()
 	var height = 150;
 	var width = 300;
 	var radius = Math.min(height, width)/2;
 
-	// data
-	var data = Object.values(data[key]);
+	// data alle buurtcombina's filteren naar één buurtcombinatie
+	var values = Object.values(data[data_type])
+	var keys = Object.keys(data[data_type])
+	var data = keys.map(function(d){return {label: d, value:data[data_type][d]}})
 
-	var length = Math.min(data);
+
+	var length = Math.min(values);
 
 	// color scale
-	var color_scale = d3.scale.ordinal().range([1, 64]).range(["#FF0000", "#009933" , "#0000FF"]);
+	var color_scale = d3.scale.ordinal().range([1, 64]).range(["#007bff", "#00FFFF", "#ee82ee", "#ffff00", "#ff0000", "#00cc00"]);
 
 	var color = d3.scale.linear().domain([1,length])
       .interpolate(d3.interpolateHcl)
@@ -113,92 +157,176 @@ function piechart(data, svg_piechart, key ) {
 
 	// define areas
 	var arc = d3.svg.arc()
-		.innerRadius(0)
-		.outerRadius(radius - 10)
+		.outerRadius(radius * 0.8)
+		.innerRadius(radius * 0.4);
+
+	var outerArc = d3.svg.arc()
+		.innerRadius(radius * 0.9)
+		.outerRadius(radius * 0.9);
 
 	var pie = d3.layout.pie()
 		.sort(null)
-		.value(function(d){ return(d)})
+		.value(function(d){ return(d.value)})
 	
-	var svg_piechart = svg_piechart.append('g')
+	var container = svg_piechart.append('g')
 		.attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')')
 		.attr('width', width)
 		.attr('height', height)
 		.attr('class', 'piechart_container')
 
-	var g = svg_piechart.selectAll('.arc')
+	var label = function(d){ return d.data.label; };
+
+	var arc_container = container.append('g')
+		.attr('class', 'arc_container')
+
+	var charts = arc_container.selectAll('.arc')
 		.data(pie(data))
 		.enter()
 		.append('g')
 		.attr('class', 'arc')
 
-	g.append('path')
+	var lines = container.append('g')
+		.attr('class', 'lines_container')
+
+	var labels = container.append('g')
+		.attr('class', 'labels')
+
+
+	var polylines = lines.selectAll('polyline')
+		.data(pie(data), label)
+		.enter()
+		.append('polyline')
+		.attr('class', 'polylines');
+
+	// Source: http://bl.ocks.org/dbuezas/9306799 (het tekenen van de lijnen bij de chart )
+
+	var text = labels.selectAll("text")
+		.data(pie(data), label);
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.text(function(d) {
+			return d.data.label;
+		});
+	
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.transition().duration(1000)
+		.attrTween("transform", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+
+	polylines.transition().duration(1000)
+	.attrTween("points", function(d){
+		this._current = this._current || d;
+		var interpolate = d3.interpolate(this._current, d);
+		this._current = interpolate(0);
+		return function(t) {
+			var d2 = interpolate(t);
+			var pos = outerArc.centroid(d2);
+			pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+			return [arc.centroid(d2), outerArc.centroid(d2), pos];
+		};
+	});
+
+	charts.append('path')
 	.attr('d', arc)
-	.style("fill", function(d) { return(color_scale(d.data)); });
+	.style("fill", function(d) { return(color_scale(d.data.value)); });
 		
 }
 
-// **********
+// ****************
 // OTHER FUNCTIONs
-// **********
+// ****************
 
-function bereken_kaart_scale(data, key) {
-	var values = Object.values(data).map(function(i){return i[key]})
+function bereken_scale(data, data_type) {
+	var values = Object.values(data).map(function(i){return i[data_type]})
 	var max = d3.max(values)
 	var min = d3.min(values)
-	return d3.scale.linear().domain([min, max]).range(['#e5f5f9', '#99d8c9', '#2ca25f'])
+	return d3.scale.linear().domain([min, max]).range(['#ffffff', '#007bff'])
 }
 
 // ****************
 // CALLBACKs
 // *****************
 
-
 d3.json('../data/buurten_amsterdam_wsg84_stadsdelen_zip.geojson', function (err, data_geo) {
 	if(err) console.log("error fetching data");
+
 	// maak groep ('g') en bind data
-	var buurten = svg_kaart.selectAll('g')
+	buurten = svg_kaart.selectAll('g')
 		.data(data_geo.features)
 		.enter()
 		.append('g')
 		.attr('class', 'buurt')
-		//.attr('id', function(d){return(d)})
 
 	d3.json('../data/price_reviews.json', function(data_buurten) {
 
-
 		d3.json('../data/property_types.json', function(property_types) {
 
-			// Dropdown
+		// ********************
+		// dropdown
+		// ********************
+
 			d3.select('#dropdown')
 				.on('change', function() {
-					var key = d3.select(this)[0][0].value
-					kaart(data_buurten, buurten, key);
-					histogram(data_buurten, svg_histogram, key);
+					data_type = d3.select(this)[0][0].value;
+					kaart(data_buurten, buurten, data_type);
+					histogram(data_buurten, svg_histogram, data_type);
+					console.log(buurten)
+					Buurten()
 
 			});
 
-			// mouseselect buurten
+			function Buurten(){
+				console.log(buurten)
+			}
 		
-			// *******************
-			// mouseover show data
-			// *******************
-
-			buurten.on('mouseover', function(d) {
-
-				postcode = d.properties.postcode
-				data = data_buurten[postcode][key]
-				show_data.text(data)
-			// link between graphs
-				var bin = d3.selectAll('.bin')
-				color_scale = bereken_kaart_scale(data_buurten, key)
-				d3.select('#pc'+postcode).classed("highlighted", true).attr('fill', '#FF0000')
-				d3.select(this).classed("highlighted_kaart", true).select('path').attr('fill', '#FF0000')
-			})
+		// ********************
+		// mouseover show data
+		// ********************
 
 			buurten.on("mouseout", function(d) {
-				d3.select(".highlighted_kaart").classed("highlighted_kaart", false).select('path').attr('fill', function(d) {return color_scale(data_buurten[d.properties.postcode][key]) })
-				d3.select(".highlighted").classed("highlighted", false).attr('fill', function(postcode) { return color_scale(data_buurten[postcode][key])});
+				d3.select(".highlighted_kaart").classed("highlighted_kaart", false).select('path').attr('fill', function(d) {return color_scale(data_buurten[d.properties.postcode][data_type]) })
+				d3.select(".highlighted_histogram").classed("highlighted_histogram", false).attr('fill', function(postcode) { return color_scale(data_buurten[postcode][data_type])});
+				show_buurt.text(' ')
+				show_data.text(' ')
+			})
+
+			buurten.on('mouseover', function(d) {
+				postcode = d.properties.postcode
+
+			// verander data tekst
+				data = data_buurten[postcode][data_type]
+				show_data.text(data)
+			// verander buurt tekst
+				buurt_tekst = d.properties.buurtcom00
+				show_buurt.text(buurt_tekst)
+			// kaart & histogram highlights
+				var bin = d3.selectAll('.bin')
+				color_scale = bereken_scale(data_buurten, data_type)
+				d3.select('#pc'+postcode).classed("highlighted_histogram", true).attr('fill', '#FF0000')
+				var red = d3.select(this).classed("highlighted_kaart", true).select('path').attr('fill', '#FF0000')
 			})
 
 			// **********
@@ -207,27 +335,38 @@ d3.json('../data/buurten_amsterdam_wsg84_stadsdelen_zip.geojson', function (err,
 
 			d3.selectAll('.buurt').on('click', function(data) {
 
-
-				console.log(postcode)
 				// PIECHART
 				var postcode = data.properties.postcode;
 				piechart(property_types, svg_piechart1, postcode);
 
 				// KLEUR SELECTIE
-				var color_scale = bereken_kaart_scale(data_buurten, key);
-				d3.select(".selected").classed("selected", false).select('path').attr('fill', function(d) {return color_scale(data_buurten[d.properties.postcode][key]) });
-				d3.select(this).classed('selected', true).attr('fill', '#FF5733')
+				if (selected != 0) {
+					// kleur de graph weer terug naar blauw. 
+					if (postcode == selected) {
+						// Dubbel muisklik a.k.a. selectie is hetzelfde
+						d3.select(".selected").classed("selected", false).select('path').attr('fill', function(d) {return color_scale(data_buurten[d.properties.postcode][data_type]) });
+						selected = 0
+					} else {
+						// selectie is niet hetzelfde 
+						
+					}
+				} else {
+					// zet de variabele terug naar 0 als er twee keer geklikt is. 
+					selected = 0
+				}
+				//console.log(postcode, d3.select('.selected').select('path')[0][0].id)
+				var color_scale = bereken_scale(data_buurten, data_type);
+				d3.select(".selected").classed("selected", false).select('path').attr('fill', function(d) {return color_scale(data_buurten[d.properties.postcode][data_type]) });
+				selected = d3.select(this).classed('selected', true).attr('fill', '#FF5733').select('path')[0][0].id
 
-
-				//d3.select('.selected').select('path').property('__data__').properties.postcode
 			});
 
 			// *********
 			// Load
 			// *********
 
-			kaart(data_buurten, buurten, key);
-			histogram(data_buurten, svg_histogram, key);
+			kaart(data_buurten, buurten, data_type);
+			histogram(data_buurten, svg_histogram, data_type);
 			piechart(property_types, svg_piechart1, '1017');
 			piechart(property_types, svg_piechart2, '1017');
 
